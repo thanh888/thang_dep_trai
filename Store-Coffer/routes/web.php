e66e520747e860ec5be0e7e5c1e13a68cdb8ef6f
thanh888 <nguyentuthanh032002@gmail.com>
2021-11-21 16:48:05 +0700

    hhh

---
+++ Store-Coffer/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// --Frontend---
+Route::get('/', [HomeController::class, 'index'])->name('trangchu');
+
+
+// --backend---
+Route::get('/dashboard', [AdminController::class,'index']);
+
